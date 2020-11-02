428a6815ba42746fd3e2aa1c8d2689cb475dd072
uzinok <uzinok@yandex.ru>
2020-11-02 14:29:41 +0400

    добавил babel

--- gulp/js.js
+++ gulp/js.js
@@ -1,18 +1,22 @@
 var gulp = require("gulp"),
     concat = require("gulp-concat"),
-    minify = require('gulp-minify');
+    minify = require('gulp-minify'),
+    babel = require('gulp-babel');
 
-gulp.task("js", function () {
-  return gulp.src("src/js/*.js")
-    .pipe(concat("main.js", {
-      newLine: ";"
-    }))
-    .pipe(minify({
-      ext: {
-        src: '-debug.js',
-        min: '.js'
-      },
-      exclude: ['tasks']
-    }))
-    .pipe(gulp.dest("build/js"));
-});
+    gulp.task("js", function () {
+      return gulp.src("src/js/*.js")
+        .pipe(concat("main.js", {
+          newLine: ";"
+        }))
+        .pipe(babel({
+          presets: ["@babel/preset-env"]
+        }))
+        .pipe(minify({
+          ext: {
+            src: '.js',
+            min: '.min.js'
+          },
+          exclude: ['tasks']
+        }))
+        .pipe(gulp.dest("build/js"));
+    });
